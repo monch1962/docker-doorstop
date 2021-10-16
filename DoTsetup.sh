0bc43b9e7b04311d8b0987e7f5807de724e08464
David Mitchell <monch1962@gmail.com>
2021-10-17 05:29:55 +1100

    Added PROCESS, PEOPLE, DESIGN into tree

--- DoTsetup.sh
+++ DoTsetup.sh
@@ -27,3 +27,53 @@ doorstop add -n ID123 INSTANCE
 doorstop link INSTANCE-ID123 SVC-AWSRDS
 doorstop add -n ID124 INSTANCE
 doorstop link INSTANCE-ID124 SVC-AWSFARGATE
+
+# Add high-level design principles
+doorstop add -n HIGHAVAILABILITY DESIGN
+doorstop add -n DATASOVEREIGNTY DESIGN
+
+DS_PATH='./items/design'
+SVCNAME='HIGHAVAILABILITY'
+SVCDESCRIPTION=$(cat <<-END
+|
+  # High availability
+  ## Delivered through leveraging AWS' Availability Zones (AZs)
+
+  (some text)
+  
+  
+  <https://aws.amazon.com/kms/>
+END
+) \
+yq eval -i '.text = env(SVCDESCRIPTION)' ${DS_PATH}/DESIGN-${SVCNAME}.yml
+
+SVCNAME='DATASOVEREIGNTY'
+SVCDESCRIPTION=$(cat <<-END
+|
+  # Data sovereignty
+  ## Delivered through leveraging AWS' Availability Zones (AZs)
+
+  AWS customers choose the AWS Region or Regions in which their content and servers will be located. This allows customers with geographic specific requirements to establish environments in a location or locations of their choice. For example, AWS customers in Australia can choose to deploy their AWS services exclusively in one AWS Region such as the Asia Pacific (Sydney) Region and store their content onshore in Australia, if this is their preferred location. If the customer makes this choice, AWS will not move their content from Australia without the customer’s consent, except as legally required.
+  
+  Customers always retain control of which AWS Region(s) are used to store and process content. AWS only stores and processes each customers’ content in the AWS Region(s), and using the services, chosen by the customer, and otherwise will not move customer content without the customer’s consent, except as legally required.
+  
+  
+  <https://d1.awsstatic.com/whitepapers/compliance/Using_AWS_in_the_context_of_Australian_Privacy_Considerations.pdf>
+END
+) \
+yq eval -i '.text = env(SVCDESCRIPTION)' ${DS_PATH}/DESIGN-${SVCNAME}.yml
+
+
+# Add high-level design principles
+doorstop add -n CODEMERGE PROCESS
+
+
+SVCNAME='CODEMERGE'
+SVCDESCRIPTION=$(cat <<-END
+|
+  # Code merging
+  ## Using a standard version control tool & process
+
+END
+) \
+yq eval -i '.text = env(SVCDESCRIPTION)' ${DS_PATH}/PROCESS-${SVCNAME}.yml
